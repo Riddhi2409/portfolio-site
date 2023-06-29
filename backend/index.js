2f452c186e3cb0121d9563f284277cb2392aff57
Riddhi2409 <98161523+Riddhi2409@users.noreply.github.com>
2023-06-29 14:16:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors= require('cors')
 require('dotenv').config();
 
+
 const port=process.env.PORT || 8080;
 const mailRoutes=require('./router/mail')
 
@@ -11,7 +12,7 @@ app.use(express.json({ limit: '50mb' }))
 app.use(cors())
 app.use('/mail',mailRoutes)
 
-app.listen(8080,()=>{console.log("server started")})
+app.listen(port,()=>{console.log("server started")})
 
 app.get('/',(req,res)=>{
     
